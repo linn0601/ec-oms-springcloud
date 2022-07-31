52fbb9951c8f6d010e7260bc398a1d1ba6e3e871
linn <linn0601@qq.com>
2022-07-31 22:53:22 +0800

    feat: 完善mvc模块

--- ec-oms-mvc/src/main/java/org/linn/config/WebConfig.java
+++ ec-oms-mvc/src/main/java/org/linn/config/WebConfig.java
@@ -3,18 +3,21 @@ package org.linn.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Collections;
 import org.linn.web.JsonHttpMessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
 	// 覆盖掉原有的MappingJackson2HttpMessageConverter
 	@Bean
-	public HttpMessageConverter<Object> customJsonHttpMessageConverter(ObjectMapper objectMapper){
+	public HttpMessageConverter<Object> customJsonHttpMessageConverter(@Autowired ObjectMapper objectMapper){
 
 		JsonHttpMessageConverter messageConverter = new JsonHttpMessageConverter(objectMapper);
 		messageConverter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
