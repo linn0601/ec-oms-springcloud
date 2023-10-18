dfea31c2d85e6abb0948b78433f653470d42d2ed
LINN\BAIJU <linn0601@qq.com>
2023-10-18 09:48:43 +0800

    feat: 服务注册nacos并通过admin进行监控

--- ec-oms-account/src/main/java/org/linn/AccountApplication.java
+++ ec-oms-account/src/main/java/org/linn/AccountApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class Application {
+public class AccountApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(AccountApplication.class, args);
 	}
 
 }
