8015c2b6d502d41123c5505f6c34c5911ef5a68b
linn <linn0601@qq.com>
2024-02-26 22:54:05 +0800

    log: 增加日志和http请求

--- ec-oms-authorization-center/src/main/java/org/linn/controller/AuthorityController.java
+++ ec-oms-authorization-center/src/main/java/org/linn/controller/AuthorityController.java
@@ -4,6 +4,8 @@ import org.linn.annotation.IgnoreResponseAdvice;
 import org.linn.service.JWTService;
 import org.linn.vo.JwtToken;
 import org.linn.vo.UsernamePassword;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthorityController {
+
+	private static final Logger log = LoggerFactory.getLogger(AuthorityController.class);
 
 	private final JWTService jwtService;
 
@@ -26,6 +30,7 @@ public class AuthorityController {
 	@IgnoreResponseAdvice
 	public JwtToken token(@RequestBody UsernamePassword usernamePassword) throws Exception {
 		String token = jwtService.generateToken(usernamePassword.getUsername(), usernamePassword.getPassword());
+		log.info("token: {}", token);
 		return new JwtToken(token);
 	}
 
@@ -36,6 +41,7 @@ public class AuthorityController {
 	@IgnoreResponseAdvice
 	public JwtToken register(@RequestBody UsernamePassword usernamePassword) throws Exception {
 		String token = jwtService.registerUserAndGenerateToken(usernamePassword);
+		log.info("token: {}", token);
 		return new JwtToken(token);
 	}
 
